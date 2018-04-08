722a4bb7e4c7e5a40275b43fdd1a1ff9b8d8ba24
Javier Fernandez <ljavierfm@gmail.com>
2018-04-08 18:39:22 +0200

    uppercase,lowercase,slice,number,percent,currency,json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre='Javier';
+  arreglo=[1,2,3,4,5,6,7,8,9,10];
+
+  PI:number=Math.PI;
+
+  a:number=0.234;
+
+  salario=1234.5;
+
+  heroe={
+    nombre:'Logan',
+    clave:'Wolverine',
+    edad:500,
+    direccion:{
+      calle:'Primera',
+      casa:19
+    }
+  }
 }
+ 
